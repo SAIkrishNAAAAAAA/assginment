40f6a059452e151417f88cfb3cd08cd6e8e0d395
SAIkrishNAAAAAAA <136511234+SAIkrishNAAAAAAA@users.noreply.github.com>
2023-06-28 00:26:59 +0530

    Add files via upload

---
+++ assig27_6_2023.py
@@ -0,0 +1,28 @@
+
+# def sum(numbers):
+#     total = 0
+#     for x in numbers:
+#         total += x
+#     print( total)
+
+
+# numbers =list(map(int,input("enter a number:").split()))
+# sum(numbers)
+
+
+ 
+
+
+
+
+
+
+
+
+def reverse(string):
+    reverse = string[::-1]
+    print("retun",reverse)
+
+string = input("Enter a string:")    
+reverse(string)
+
